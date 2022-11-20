656cfade25c135d8f142702ede7223b9c631f53d
Svitlana Cherniakova <107955029+Svchernn@users.noreply.github.com>
2022-11-20 21:35:14 +0200

    video-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,10 +2,18 @@ import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+const player = new Player(iframe);
 
-    player.on('timeupdate', throttle(onPlay, 1000) 
-       );
+player.on(
+  'timeupdate',
+  throttle(data => onPlay(data), 1000)
+);
 
-   
-    
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}
+
+const currentTime =
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))?.seconds ?? 0;
+
+player.setCurrentTime(currentTime);
